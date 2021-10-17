a6592deb673bffc9f906ca0e8219d63f9a11cad4
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2021-10-17 16:52:00 +0800

    Merge MainController and MainActivity into one file

--- app/src/main/java/com/example/popsicle/MainActivity.java
+++ app/src/main/java/com/example/popsicle/MainActivity.java
@@ -3,15 +3,50 @@ package com.example.popsicle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.popsicle.io.InputHandler;
+import com.example.popsicle.io.InputListener;
+import com.example.popsicle.io.MoveCharacterAction;
+import com.example.popsicle.models.Universe;
+import com.example.popsicle.rendering.GraphicsRenderer;
+
+public class MainActivity extends AppCompatActivity{
+
+
+    private static final String TAG = "MainActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate Started");
+
+        // Using surfaceView
         SurfaceView sv = findViewById(R.id.surfaceView);
-        MainController mc = new MainController(sv);
+
+        // Creating the universe
+        Universe universe = new Universe();
+
+        // Rendering the assets
+        GraphicsRenderer graphicsRenderer = new GraphicsRenderer(universe, sv.getResources());
+        universe.setCallBack(graphicsRenderer);
+        sv.setWillNotDraw(false);
+        sv.getHolder().addCallback(graphicsRenderer);
+
+
+        InputListener inputListener = new InputListener();
+        sv.setOnTouchListener(inputListener);
+
+        InputHandler inputHandler = new InputHandler();
+        inputHandler.setOnClickAction(new MoveCharacterAction(universe));
+        inputListener.setCallback(inputHandler);
+
+        Log.d(TAG, "onCreate Finished");
+
     }
+
 }
