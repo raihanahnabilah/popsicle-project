5783547a56bda8f2a451349addd9d9b481bf2106
Valeria <e0435489@u.nus.edu>
2021-10-23 00:09:18 +0800

    Syrup class comments, documentation

--- app/src/main/java/com/example/popsicle/models/Syrup.java
+++ app/src/main/java/com/example/popsicle/models/Syrup.java
@@ -6,6 +6,9 @@ import android.graphics.BitmapFactory;
 
 import com.example.popsicle.R;
 
+/*
+* Class representing what is 'shot' from the clouds to make the enemy character loose lives
+ */
 public class Syrup {
     private SyrupPosition pos;
     String clouds;
@@ -15,14 +18,26 @@ public class Syrup {
     // Global object to update the universe -- to keep all of the elements updated
     // Direction inside the syrup object
 
+    /*
+    * Creates a new SyrupPosition for the Syrup
+    * @param x assign the initial x coordinate location of the syrup drop
+    * @param y assign the initial y coordinate location of the syrup drop
+     */
     public Syrup(float x, float y) {
         this.pos = new SyrupPosition(x,y);
     }
 
+    /*
+    * Updates the location of the drop for it to move
+    * @param m position coordinates
+     */
     public void move(SyrupMotion m) {
         this.pos.add(m);
     }
 
+    /*
+    * Gets the current Syrup position
+    */
     public SyrupPosition getPosition() {
         return this.pos;
     }
