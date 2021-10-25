af9d4f3328e23bc902db225b85037fc5e31a7128
James Pierog <james.pierog@gmail.com>
2021-10-25 17:47:56 +0800

    added firebase connection and demo firebase thing

--- app/src/main/java/com/example/popsicleproject/JoinGame.java
+++ app/src/main/java/com/example/popsicleproject/JoinGame.java
@@ -1,14 +1,57 @@
 package com.example.popsicleproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class JoinGame extends AppCompatActivity {
+
+    TextView number;
+    Button updateButton2;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = mRootRef.child("number");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_game);
+
+        number = (TextView) findViewById(R.id.number);
+        updateButton2 = (Button) findViewById(R.id.updateButton2);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String text = snapshot.getValue(String.class);
+                number.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        updateButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mConditionRef.setValue("RESET THE BUTTON");
+            }
+        });
     }
 }
