a665541b4bb8991b079ce754938677fdb5aae735
Hana Nabilah Fatinah <raihanahnabilah@gmail.com>
2021-10-30 05:22:54 +0800

    Creating Home Page activity and the Background class

---
+++ app/src/main/java/com/example/popsicle/HomePage.java
@@ -0,0 +1,27 @@
+package com.example.popsicle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this,MainActivity.class));
+            }
+        });
+
+    }
+}
