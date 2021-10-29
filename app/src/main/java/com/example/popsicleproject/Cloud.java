73a8b61003179d7c251198bc75e4533a5893a6ec
valeriaebs28 <v.barcosantana@u.yale-nus.edu.sg>
2021-10-30 05:14:21 +0800

    Optimized Cloud objects location
    All clouds' initial location has been modified to contain no variables and rely on the device screen

--- app/src/main/java/com/example/popsicleproject/Cloud.java
+++ app/src/main/java/com/example/popsicleproject/Cloud.java
@@ -22,20 +22,22 @@ public class Cloud {
         width /= 20;
         height /= 20;
 
+//        this are the screens width and height
+//        screen_width = MainActivity.window
         cloud = Bitmap.createScaledBitmap(cloud, width, height, false);
 
         if (direction == "a1"){
-            x = 550;
-            y = (screenY /2) - 500;
+            x = screenX/4;
+            y = (screenY /64);
         } else if (direction == "a2"){
-            x = 250;
-            y = (screenY /2) + 200;
+            x = screenX/8;
+            y = (screenY *7)/10;
         } else if (direction == "b1"){
-            x = screenX - 450;
-            y = (screenY /2) - 500;
+            x = (screenX *3)/4;
+            y = (screenY /60);
         } else if (direction == "b2"){
-            x = screenX - 850;
-            y = (screenY /2) + 200;
+            x = screenX /2;
+            y = (screenY *3)/4;
         }
 
     }
