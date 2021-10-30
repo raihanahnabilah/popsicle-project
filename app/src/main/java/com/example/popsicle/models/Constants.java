63d71301e7ecb45ad0b1b873ad13cda5041f1820
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2021-10-30 23:50:02 +0800

    Adding constants for the pictures' bitmap width and height

--- app/src/main/java/com/example/popsicle/models/Constants.java
+++ app/src/main/java/com/example/popsicle/models/Constants.java
@@ -3,6 +3,14 @@ package com.example.popsicle.models;
 public class Constants {
     public static int syrupMovementPixelsX = 20;
     public static int charMovementPixels = 20;
-//    public int toShoot = 0;
-//    public int toShootA2 = 0;
+    public static int charWidth = 176;
+    public static int charHeight = 176;
+    public static int cloudWidth = 286;
+    public static int cloudHeight = 286;
+    public static int popsicleWidth = 137;
+    public static int popsicleHeight = 137;
+    public static int syrupWidth = 70;
+    public static int syrupHeight = 70;
+    public static int consoleWidth = 140;
+    public static int consoleHeight = 140;
 }
