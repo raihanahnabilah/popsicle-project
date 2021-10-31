9cc094d2dd872400c0def0e5b3cd917e63b7dc15
valeriaebs28 <v.barcosantana@u.yale-nus.edu.sg>
2021-11-01 07:34:50 +0800

    Completed TestCandy

--- app/src/test/java/com/example/popsicle/TestCandy.java
+++ app/src/test/java/com/example/popsicle/TestCandy.java
@@ -1,5 +1,7 @@
 package com.example.popsicle;
 
+import android.graphics.Rect;
+
 import com.example.popsicle.models.Candy;
 import com.example.popsicle.models.Clouds;
 import com.example.popsicle.models.Constants;
@@ -8,8 +10,6 @@ import com.example.popsicle.models.Position;
 import org.junit.Test;
 
 public class TestCandy {
-
-    //TODO: Finish the remaining testings.
 
     Candy a = new Candy("a");
     Candy b = new Candy("b");
@@ -32,12 +32,34 @@ public class TestCandy {
 
     @Test
     public void PopsiclesCollisionShape(){
-        //TODO: Testing the Popsicles' collision shape
+          int a_left = (int) candyAPos.getX()*33/32;
+          int a_top = (int) candyAPos.getY()*33/32;
+          int a_right = (int) (candyAPos.getX() + candyWidth)*31/32;
+          int a_bottom = (int) (candyAPos.getY() + candyHeight)*61/64;
+          Rect rec_a = new Rect(a_left, a_top, a_right, a_bottom);
+          assert (rec_a.left == a.getCollisionShape().left);
+          assert (rec_a.right == a.getCollisionShape().right);
+          assert (rec_a.bottom == a.getCollisionShape().bottom);
+          assert (rec_a.top == a.getCollisionShape().top);
+
+          int b_left = (int) candyBPos.getX()*33/32;
+          int b_top = (int) candyBPos.getY()*33/32;
+          int b_right = (int) (candyBPos.getX() + candyWidth)*31/32;
+          int b_bottom = (int) (candyBPos.getY() + candyHeight)*61/64;
+          Rect rec_b = new Rect(b_left, b_top, b_right, b_bottom);
+          assert (rec_b.left == b.getCollisionShape().left);
+          assert (rec_b.right == b.getCollisionShape().right);
+          assert (rec_b.bottom == b.getCollisionShape().bottom);
+          assert (rec_b.top == b.getCollisionShape().top);
     }
 
     @Test
     public void PopsiclesWidthAndHeight(){
-        //TODO: Testing the Popsicles' width and height
+        assert (a.getHeight() == candyHeight);
+        assert (a.getWidth() == candyWidth);
+
+        assert (b.getHeight() == candyHeight);
+        assert (b.getWidth() == candyWidth);
     }
 
 }
