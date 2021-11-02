f8697c0ec7399b11cafb6a218d9c9075123aeec7
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2021-11-02 23:00:05 +0800

    Create GameWon Activity page

---
+++ app/src/main/java/com/example/popsicle/GameWon.java
@@ -0,0 +1,23 @@
+package com.example.popsicle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class GameWon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_won);
+
+        findViewById(R.id.homepage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GameWon.this,HomePage.class));
+            }
+        });
+    }
+}
