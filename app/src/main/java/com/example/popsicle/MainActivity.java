42d319cf8840a9a752e41c15864ace47e1e82fe7
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2021-11-02 23:04:41 +0800

    Commented out the onPause() method on MainActivity to avoid crashing when game is terminated to the GameOver or GameWon activity page

--- app/src/main/java/com/example/popsicle/MainActivity.java
+++ app/src/main/java/com/example/popsicle/MainActivity.java
@@ -64,10 +64,10 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
-    @Override
-    protected void onPause(){
-        super.onPause();
-        backgroundMusic.release();
-        finish();
-    }
+//    @Override
+//    protected void onPause(){
+//        super.onPause();
+//        backgroundMusic.release();
+//        finish();
+//    }
 }
