3e512af7327f178d179af77aba6d504eb1216b0a
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2021-11-02 23:03:56 +0800

    Set GameOver and GameWon activity page to be full screen

--- app/src/main/java/com/example/popsicle/GameWon.java
+++ app/src/main/java/com/example/popsicle/GameWon.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class GameWon extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class GameWon extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_won);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         findViewById(R.id.homepage).setOnClickListener(new View.OnClickListener() {
             @Override
