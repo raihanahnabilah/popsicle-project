d8189f8c3604a62afdbeb83dfd9e1ab6ff9a527f
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2021-11-02 11:48:35 +0800

    Create whichPlayer Java to get the Console

---
+++ app/src/main/java/com/example/popsicle/models/whichPlayer.java
@@ -0,0 +1,13 @@
+package com.example.popsicle.models;
+
+public class whichPlayer {
+
+    public static Boolean amIPlayerA = false;
+    public static Boolean amIPlayerB = false;
+
+    public whichPlayer(Boolean amIPlayerA, Boolean amIPlayerB){
+        this.amIPlayerA = amIPlayerA;
+        this.amIPlayerB = amIPlayerB;
+    }
+
+}
